e986628f84d5d7a565f94fd914adf8cbd89c9a3c
Jonas Primbs <mail@jonasprimbs.de>
2022-04-25 18:39:20 +0200

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'oidc-iat-demo'`, () => {
+  it(`should have as title 'oidc-iat-debugger'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('oidc-iat-demo');
+    expect(app.title).toEqual('oidc-iat-debugger');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('oidc-iat-demo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('oidc-iat-debugger app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { base64ToBase64url, base64urlToBase64, decodeBase64url, encodeBase64url } from '@jonasprimbs/byte-array-converter';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,336 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'oidc-iat-demo';
+  private _iatHeaderString: string = '{"alg":"ES384","typ":"JWT"}';
+  private _iatPayloadString: string = '{"iss":"https://jonasprimbs.de/","sub":"1234567890","name":"John Doe","iat":123456789}';
+  private _iatSignatureString: string = '';
+
+  iatSignatureValid?: Promise<boolean | undefined>;
+
+  private _messagePayload: string = 'Sample message';
+  private _messageSignature: string = '';
+  messageSignatureValid?: Promise<boolean | undefined>;
+
+  cnfPrivateKey?: JsonWebKey;
+
+  opPublicKey?: JsonWebKey;
+  opPrivateKey?: JsonWebKey;
+
+  constructor() {
+    this.generateOpKeyPair();
+    this.generateCnfKeyPair();
+  }
+
+  get iatSignatureString(): string {
+    return this._iatSignatureString;
+  }
+  set iatSignatureString(value: string) {
+    this._iatSignatureString = value;
+    if (this.opPublicKey) {
+      this.verifyIatSignature();
+    }
+  }
+
+  get iatHeaderString(): string {
+    return this._iatHeaderString;
+  }
+  set iatHeaderString(value: string) {
+    this._iatHeaderString = value;
+    if (this.opPrivateKey) {
+      this.signIat();
+    }
+  }
+
+  get iatPayloadString(): string {
+    return this._iatPayloadString;
+  }
+  set iatPayloadString(value: string) {
+    this._iatPayloadString = value;
+    if (this.opPrivateKey) {
+      this.signIat();
+    }
+  }
+
+  get message(): string {
+    return [
+      this.toBase64Url(this.messagePayload),
+      base64ToBase64url(this.messageSignature),
+    ].join('.');
+  }
+  set message(value: string) {
+    const parts = value.split('.');
+    this.messagePayload = this.fromBase64Url(parts[0] ?? '');
+    this.messageSignature = base64urlToBase64(parts[1] ?? '');
+  }
+
+  get messageSignature(): string {
+    return this._messageSignature;
+  }
+  set messageSignature(value: string) {
+    this._messageSignature = value;
+    if (this.cnfPublicKey) {
+      this.verifyMessageSignature();
+    }
+  }
+
+  get messagePayload(): string {
+    return this._messagePayload;
+  }
+  set messagePayload(value: string) {
+    this._messagePayload = value;
+    if (this.cnfPrivateKey) {
+      this.signMessage();
+    }
+  }
+
+  get idAssertionToken(): string {
+    return [
+      this.toBase64Url(this.iatHeaderString),
+      this.toBase64Url(this.iatPayloadString),
+      encodeURI(this.iatSignatureString),
+    ].join('.');
+  }
+  set idAssertionToken(value: string) {
+    const parts = value.split('.');
+    this.iatHeaderString = this.fromBase64Url(parts[0] ?? '');
+    this.iatPayloadString = this.fromBase64Url(parts[1] ?? '');
+    this.iatSignatureString = decodeURI(parts[2] ?? '');
+  }
+
+  get iatHeader(): { [claim: string]: any } {
+    return JSON.parse(this.iatHeaderString) ?? {};
+  }
+  set iatHeader(value: { [claim: string]: any }) {
+    this.iatHeaderString = JSON.stringify(value);
+  }
+
+  get iatPayload(): { [claim: string]: any } {
+    return JSON.parse(this.iatPayloadString) ?? {};
+  }
+  set iatPayload(value: { [claim: string]: any }) {
+    this.iatPayloadString = JSON.stringify(value);
+  }
+
+  get issuerUri(): string {
+    return this.iatPayload?.['iss'] ?? '';
+  }
+  set issuerUri(value: string) {
+    let payload = this.iatPayload;
+    payload['iss'] = value;
+    this.iatPayload = payload;
+  }
+
+  get opPublicKeyString(): string {
+    if (this.opPublicKey) {
+      return JSON.stringify(this.opPublicKey);
+    } else {
+      return '';
+    }
+  }
+  set opPublicKeyString(value: string) {
+    if (value) {
+      this.opPublicKey = JSON.parse(value);
+    } else {
+      this.opPublicKey = undefined;
+    }
+  }
+
+  get opPrivateKeyString(): string {
+    if (this.opPrivateKey) {
+      return JSON.stringify(this.opPrivateKey);
+    } else {
+      return '';
+    }
+  }
+  set opPrivateKeyString(value: string) {
+    if (value) {
+      this.opPrivateKey = JSON.parse(value);
+    } else {
+      this.opPrivateKey = undefined;
+    }
+  }
+
+  get cnfPublicKey(): JsonWebKey | undefined {
+    return this.iatPayload['cnf'];
+  }
+  set cnfPublicKey(value: JsonWebKey | undefined) {
+    const payload = this.iatPayload;
+    payload['cnf'] = value;
+    this.iatPayload = payload;
+  }
+
+  get cnfPublicKeyString(): string {
+    if (this.cnfPublicKey) {
+      return JSON.stringify(this.cnfPublicKey);
+    } else {
+      return '';
+    }
+  }
+  set cnfPublicKeyString(value: string) {
+    if (value) {
+      this.cnfPublicKey = JSON.parse(value);
+    } else {
+      this.cnfPublicKey = undefined;
+    }
+  }
+
+  get cnfPrivateKeyString(): string {
+    if (this.cnfPrivateKey) {
+      return JSON.stringify(this.cnfPrivateKey);
+    } else {
+      return '';
+    }
+  }
+  set cnfPrivateKeyString(value: string) {
+    if (value) {
+      this.cnfPrivateKey = JSON.parse(value);
+    } else {
+      this.cnfPrivateKey = undefined;
+    }
+  }
+
+  private toBase64Url(value: string): string {
+    const enc = new TextEncoder();
+    return encodeBase64url(enc.encode(value));
+  }
+  private fromBase64Url(value: string): string {
+    const dec = new TextDecoder();
+    return dec.decode(decodeBase64url(value));
+  }
+
+  private async verifySignature(data: string, signature: string, publicKey: CryptoKey): Promise<boolean> {
+    const enc = new TextEncoder();
+    const dataBuffer = enc.encode(data);
+    const signatureBuffer = decodeBase64url(signature);
+    return await window.crypto.subtle.verify(
+      {
+        name: 'ECDSA',
+        hash: {name: 'SHA-384'},
+      },
+      publicKey,
+      signatureBuffer,
+      dataBuffer,
+    );
+  }
+
+  async iatSignatureValidity(): Promise<boolean | undefined> {
+    if (!this.opPublicKey) {
+      return undefined;
+    }
+    const publicKey = await window.crypto.subtle.importKey(
+      'jwk',
+      this.opPublicKey,
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-384',
+      },
+      true,
+      ['verify'],
+    );
+    const iatData = [
+      this.toBase64Url(this.iatHeaderString),
+      this.toBase64Url(this.iatPayloadString),
+    ].join('.');
+    return await this.verifySignature(iatData, this.iatSignatureString, publicKey);
+  }
+
+  verifyIatSignature(): void {
+    this.iatSignatureValid = this.iatSignatureValidity();
+  }
+
+  async messageSignatureValidity(): Promise<boolean | undefined> {
+    if (!this.cnfPublicKey) {
+      return undefined;
+    }
+    const publicKey = await window.crypto.subtle.importKey(
+      'jwk',
+      this.cnfPublicKey,
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-384',
+      },
+      true,
+      ['verify'],
+    );
+    return await this.verifySignature(this.messagePayload, this.messageSignature, publicKey);
+  }
+
+  verifyMessageSignature(): void {
+    this.messageSignatureValid = this.messageSignatureValidity();
+  }
+
+  private async generateKeyPair(): Promise<CryptoKeyPair> {
+    return await window.crypto.subtle.generateKey(
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-384',
+      },
+      true,
+      ['sign', 'verify']
+    );
+  }
+
+  async generateCnfKeyPair(): Promise<void> {
+    const keyPair = await this.generateKeyPair();
+    this.cnfPrivateKey = await window.crypto.subtle.exportKey('jwk', keyPair.privateKey);
+    this.cnfPublicKey = await window.crypto.subtle.exportKey('jwk', keyPair.publicKey);
+  }
+
+  async generateOpKeyPair(): Promise<void> {
+    const keyPair = await this.generateKeyPair();
+    this.opPrivateKey = await window.crypto.subtle.exportKey('jwk', keyPair.privateKey);
+    this.opPublicKey = await window.crypto.subtle.exportKey('jwk', keyPair.publicKey);
+  }
+
+  private async generateSignature(data: string, privateKey: CryptoKey): Promise<string> {
+    const enc = new TextEncoder();
+    const dataBuffer = enc.encode(data);
+    const signatureBuffer = await window.crypto.subtle.sign(
+      {
+        name: 'ECDSA',
+        hash: {name: 'SHA-384'},
+      },
+      privateKey,
+      dataBuffer,
+    );
+    return encodeBase64url(new Uint8Array(signatureBuffer));
+  }
+
+  async signMessage(): Promise<void> {
+    if (!this.cnfPrivateKey) return;
+    const privateKey = await window.crypto.subtle.importKey(
+      'jwk',
+      this.cnfPrivateKey,
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-384',
+      },
+      true,
+      ['sign'],
+    );
+    this.messageSignature = await this.generateSignature(this.messagePayload, privateKey);
+  }
+
+  async signIat(): Promise<void> {
+    if (!this.opPrivateKey) return;
+    const privateKey = await window.crypto.subtle.importKey(
+      'jwk',
+      this.opPrivateKey,
+      {
+        name: 'ECDSA',
+        namedCurve: 'P-384',
+      },
+      true,
+      ['sign'],
+    );
+    const data = [
+      this.toBase64Url(this.iatHeaderString),
+      this.toBase64Url(this.iatPayloadString),
+    ].join('.');
+    this.iatSignatureString = await this.generateSignature(data, privateKey);
+  }
+
+  async copyToClipboard(text: string): Promise<void> {
+    await navigator.clipboard.writeText(text);
+  }
 }
